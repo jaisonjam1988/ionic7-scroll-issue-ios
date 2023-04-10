422514d19c7200026f2c38974c185458c6fd3edd
Jaison James <jaisonjam@gmail.com>
2023-04-10 06:30:17 +0530

    IOS displays a white screen while swiftly scrolling issue reproduction update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { STATES, FIXED_SIZE } from 'src/data/state';
+import { Config } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,10 @@ import { STATES, FIXED_SIZE } from 'src/data/state';
   imports: [IonicModule, ScrollingModule],
 })
 export class HomePage {
-
+  itemSize = 56;
   fixedSizeData = FIXED_SIZE;
   
-  constructor() {}
+  constructor(private config: Config) {
+    this.itemSize = config.get('mode') === 'ios' ? 44 : 56;
+  }
 }
